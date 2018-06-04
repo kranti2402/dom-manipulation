e202f8a9c2954b8a9247a3f516b4189628f2a858
Dil Afrin <dilafrin24@gmail.com>
2018-06-04 04:21:12 +0000

    Classwork 4

--- script.js
+++ script.js
@@ -6,3 +6,14 @@
 
 
 /* Task 4 using jQuery */
+   //Task4a
+   $('#addText4').click(function(event){
+       var paragraph = $('<p>').text('Hello World')
+       $('#task4a').append(paragraph)
+   })
+   
+   //Task 4b
+   function changeBG(event){
+       var color = event.target.innerText.toLowerCase();
+       $('body').css('background-color',color)
+   }
